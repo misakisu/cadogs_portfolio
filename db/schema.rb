f3fdacdbefeb15f8b49b87a13161c563bd682407
misakisu <che.v.jq@gmail.com>
2020-06-26 12:40:46 +0900

    [Fix]マイグレーションエラー対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_052835) do
+ActiveRecord::Schema.define(version: 2020_06_21_050305) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2020_06_25_052835) do
     t.datetime "updated_at", null: false
     t.index ["hotel_id"], name: "index_hotel_genre_relations_on_hotel_id"
     t.index ["pet_genre_id"], name: "index_hotel_genre_relations_on_pet_genre_id"
+  end
+
+  create_table "hotel_images", force: :cascade do |t|
+    t.integer "hotel_id", null: false
+    t.string "image_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "hotels", force: :cascade do |t|
