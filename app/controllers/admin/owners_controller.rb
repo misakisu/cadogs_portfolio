ed7d8dfd97b61efe4796ee5c73d5186e29393b99
misakisu <che.v.jq@gmail.com>
2020-07-04 17:23:22 +0900

    [Finish]管理者用userとownerの一覧[Update]予約確認

--- app/controllers/admin/owners_controller.rb
+++ app/controllers/admin/owners_controller.rb
@@ -1,2 +1,25 @@
 class Admin::OwnersController < ApplicationController
+  def index
+    @owners = Owner.all
+  end
+
+  def edit
+    @owner = Owner.find(params[:id])
+  end
+
+  def update
+    @owner = Owner.find(params[:id])
+    if @owner.update(owner_params)
+      redirect_to admin_owners_path
+      flash[:notice] = "更新されました"
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def owner_params
+    params.require(:owner).permit(:email, :name, :phone_number, :is_valid)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   #userのルート petsとreservationsネスト
   resource :user, only: [:show], shallow: true do
     resources :pets, only: [:index, :create, :edit, :update, :destroy]
-    resources :reservations, only: [:create, :index] do
+    resources :reservations, only: [:create] do
       post 'confirm', on: :collection
       get 'finish', on: :collection
     end
