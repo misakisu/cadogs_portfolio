221ae4dcb3b6d7019ba67155967f5244e1df7356
misakisu <che.v.jq@gmail.com>
2020-07-05 22:37:36 +0900

    [Finish]addtax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #税込計算 表示 (数値を金額形式で表示)
+  def addTax(money,rate)
+   taxed_money = (money*rate).round
+   "￥"+taxed_money.to_s(:delimited)+"(税込)"#:delimitedで3桁区切り
+  end
 end
