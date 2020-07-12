b4754456330023f89ad26ebc456098c6acd7a469
misakisu <che.v.jq@gmail.com>
2020-07-12 16:13:54 +0900

    [Update]レイアウト変更

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,7 +5,7 @@ class Reservation < ApplicationRecord
   #バリデーション
   validates :start_date, presence: { message: '入力してください' }
   validates :end_date, presence: { message: '入力してください' }
-  validates :pet_id, presence: { message: '入力してください' }
+  validates :pet_id, presence: { message: '様を入力してください' }
   validate :correct_date
 
   #チェックアウトの日程がチェックインの日程よりも前の日程で入力された場合は下記エラーを表示
