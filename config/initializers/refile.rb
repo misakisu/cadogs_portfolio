5601c8b90cc0c2ec2fadcc7892c7f51c08c86def
misakisu <che.v.jq@gmail.com>
2020-07-26 17:15:43 +0900

    [Update]refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,8 +3,8 @@ require "refile/s3"
 aws = {
   access_key_id: "<%= Rails.application.credentials.dig(:aws, :access_key_id) %>",
   secret_access_key: "<%= Rails.application.credentials.dig(:aws, :secret_access_key) %>",
-  region: ap-northeast-1,
-  bucket: cadogs-rails-bucket,
+  region: "ap-northeast-1",
+  bucket: "cadogs-rails-bucket",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
