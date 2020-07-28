9e456cd2512530d5cc3445055e5fe449cb7c5efa
misakisu <che.v.jq@gmail.com>
2020-07-28 21:57:49 +0900

    [Update]smtp設定追加

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   domain: 'gmail.com',
   port: 587,
-  user_name: 'Gmail のメールアドレス',
-  password: 'Gmail のパスワード',
+  user_name: Rails.application.credentials.gmail[:user_name],
+  password: Rails.application.credentials.gmail[:password],
   authentication: 'plain',
   enable_starttls_auto: true
 }
