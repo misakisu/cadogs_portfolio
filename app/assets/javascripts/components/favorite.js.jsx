b872fd84b51dbb961b80fc66b7a9a9c4e8f2c4e4
misakisu <[che.v.jq@gmail.com]>
2020-09-23 18:11:07 +0900

    [Update]Ajaxにて情報Post

--- app/assets/javascripts/components/favorite.js.jsx
+++ app/assets/javascripts/components/favorite.js.jsx
@@ -1,4 +1,19 @@
 class Star extends React.Component {
+  ajaxMain: function(method){
+        $.ajax({
+          type: "POST",
+          url: /hotels/favorite,                               //このURLは次でセットします
+          dataType: 'json',
+          data: {hotel_id: this.props.post},                  //このpostも次でセットします
+            success: function(data) {
+              if(data.like){                                 //ここはあとで活きてきます笑
+                this.setState({is_favorited: data.favorited});     //responseのlikedをstateのis_likedに反映させます
+              };
+                this.setState({counts: data.counts});          //responseのcountsをstateのcountsにに反映させます
+              }.bind(this),                                      //bindはthisが使えるようになるため
+        });
+    },
+
   constructor(props) {
     super(props)
     // いいねされたかどうかのステートを宣言
