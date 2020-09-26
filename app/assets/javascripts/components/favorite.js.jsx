c9a0702a0b06779f2f1339e37502a2b285acf416
misakisu <[che.v.jq@gmail.com]>
2020-09-26 11:10:41 +0900

    [Update]

--- app/assets/javascripts/components/favorite.js.jsx
+++ app/assets/javascripts/components/favorite.js.jsx
@@ -1,39 +1,28 @@
 class Star extends React.Component {
-  ajaxMain: function(method){
-        $.ajax({
-          type: "POST",
-          url: /hotels/favorite,                               //このURLは次でセットします
-          dataType: 'json',
-          data: {hotel_id: this.props.post},                  //このpostも次でセットします
-            success: function(data) {
-              if(data.like){                                 //ここはあとで活きてきます笑
-                this.setState({is_favorited: data.favorited});     //responseのlikedをstateのis_likedに反映させます
-              };
-                this.setState({counts: data.counts});          //responseのcountsをstateのcountsにに反映させます
-              }.bind(this),                                      //bindはthisが使えるようになるため
-        });
-    },
-
   constructor(props) {
     super(props)
     // いいねされたかどうかのステートを宣言
     this.state = {
+    	iconClassName : 'far-star',
       favorited: false,
     };
   }
 
-  handClick() {
+  handClick () {
+    const iconClassName = this.state.favorited? 'far-star' : 'fa-star';
     this.setState({
-    	favorited: !this.state.favorited/*squaresを反転した値*/
+    	iconClassName : iconClassName,/*現在の値を定義*/
+    	favorited: !this.state.favorited/*現在の値を反転した値*/
     });
   }
 
   render ()  {
-  	const iconName = this.state.favorited ? 'far-star' : 'fa-star';
     return (
-      <button onClick={this.handClick}>
-        <i className={'far ${iconName}'}></i>
+      <button onClick={() => this.handClick}>
+        <i className={'far ' + (this.state.favorited? 'far-star' : 'fa-star')}></i>
       </button>
     );
   }
 }
+
+
